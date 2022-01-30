f2e7b2089e0bbd50dd090ae0626ddb08794d26ce
Petr Zenin <mr@adminoid.com>
2022-01-30 20:53:52 +0300

    some tests with waffle and ethers

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,8 +4,7 @@
 
 // Solidity files have to start with this pragma.
 // It will be used by the Solidity compiler to validate its version.
-pragma solidity ^0.7.0;
-
+pragma solidity ^0.8.9;
 
 // This is the main building block for smart contracts.
 contract Token {
--- test/Token.js
+++ test/Token.js
@@ -1,19 +1,43 @@
-const { expect } = require("chai");
-const ethers = require('hardhat').ethers;
+const { expect, use } = require("chai")
+const { ethers, waffle } = require('hardhat')
+const { deployContract, solidity, provider } = waffle
+const MockUSDT = require('../artifacts/contracts/mocks/mockBUSD.sol/MockUSDT.json')
 
-describe("Token contract", function (done) {
-  // this.timeout(60000)
-
+describe("Token contract", _ => {
   it("Deployment should assign the total supply of tokens to the owner", async function () {
-    const [owner] = await ethers.getSigners();
+    const Token = await ethers.getContractFactory("Token")
+    const hardhatToken = await Token.deploy('Unit test')
 
-    const Token = await ethers.getContractFactory("Token");
+    expect(await hardhatToken.deployMessage()).to.equal("Unit test")
 
-    const hardhatToken = await Token.deploy('Unit test');
+    const [owner] = await ethers.getSigners()
+    const ownerBalance = await hardhatToken.balanceOf(owner.address)
 
-    console.log(await hardhatToken.deployMessage())
+    expect(await hardhatToken.totalSupply()).to.equal(ownerBalance)
+  })
+})
 
-    const ownerBalance = await hardhatToken.balanceOf(owner.address);
-    expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
-  }).timeout(60000);
-});
+describe("Token contract with waffle", async _ => {
+
+  it('should debug', async function () {
+    // use(solidity)
+    const accounts = await ethers.getSigners();
+    const wallet = accounts[0]
+    const walletTo = accounts[1]
+    const tokenUSDT = await deployContract(wallet, MockUSDT)
+
+    // Assigns initial balance
+    expect(await tokenUSDT.balanceOf(wallet.address) / 1000000)
+      .to.equal(5e+21)
+
+    // Transfer adds amount to destination account
+    await tokenUSDT.transfer(walletTo.address, 7)
+    expect(await tokenUSDT.balanceOf(walletTo.address)).to.equal(7)
+
+    // Transfer emits event
+    await expect(tokenUSDT.transfer(walletTo.address, 7))
+      .to.emit(tokenUSDT, 'Transfer')
+      .withArgs(wallet.address, walletTo.address, 7)
+
+  })
+})
