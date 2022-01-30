c7d6438ea50b2e7418a49b80d209d5fd931251c4
Petr Zenin <mr@adminoid.com>
2022-01-30 18:05:57 +0300

    added @openzeppelin/contracts and created mocks for usdt and busd

---
+++ contracts/mocks/mockBUSD.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MockUSDT is ERC20 {
+    constructor() ERC20("Mock BUSD", "BUSD") {
+        _mint(msg.sender, 5000000000 * 10 ** decimals());
+    }
+}
