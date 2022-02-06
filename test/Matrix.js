61ff72a822a357a9f1f4f92f15e443e283f54eba
Petr Zenin <mr@adminoid.com>
2022-02-06 19:26:23 +0300

    deposit USDT tests successful

--- test/Matrix.js
+++ test/Matrix.js
@@ -9,7 +9,7 @@ const MockUSDT = require('../artifacts/contracts/mocks/mockUSDT.sol/MockUSDT.jso
 
 describe('Deposit/withdraw BUSD and USDT with Matrix.sol', _ => {
   it('Deposit USDT', async () => {
-    const [deployerUSDT, deployerMatrix, commonWallet, walletUser] = await ethers.getSigners()
+    const [deployerUSDT, deployerMatrix, commonWallet, userWallet] = await ethers.getSigners()
 
     const tokenUSDT = await deployContract(deployerUSDT, MockUSDT)
     const tokenMatrix = await deployContract(deployerMatrix, Matrix, [
@@ -17,17 +17,21 @@ describe('Deposit/withdraw BUSD and USDT with Matrix.sol', _ => {
       commonWallet.address,
     ])
 
-    await tokenUSDT.connect(deployerUSDT).transfer(walletUser.address, 11)
-    let balance = await tokenUSDT.balanceOf(walletUser.address)
+    await tokenUSDT.connect(deployerUSDT).transfer(userWallet.address, 11)
+    let balance = await tokenUSDT.balanceOf(userWallet.address)
     expect(balance).to.equal(11) // ok
 
-    await tokenUSDT.connect(walletUser).approve(tokenMatrix.address, 9);
-    await tokenMatrix.connect(walletUser).depositUSDT(9)
+    await tokenUSDT.connect(userWallet).approve(tokenMatrix.address, 9);
+    await tokenMatrix.connect(userWallet).depositUSDT(9)
 
-    let usdtBalance = await tokenUSDT.balanceOf(walletUser.address)
+    let usdtBalance = await tokenUSDT.balanceOf(userWallet.address)
     expect(usdtBalance).to.equal(2)
 
-    let mxBalance = await tokenUSDT.balanceOf(commonWallet.address)
-    expect(mxBalance).to.equal(9)
+    let mxTokenBalance = await tokenUSDT.balanceOf(commonWallet.address)
+    expect(mxTokenBalance).to.equal(9)
+
+    let mxUserBalance = await tokenMatrix.balanceOf(userWallet.address)
+    expect(mxUserBalance).to.equal(9)
+
   }).timeout(20000)
 })
