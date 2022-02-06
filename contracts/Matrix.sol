8cca4abb05b85c047261b71b7f51d7dfdceea944
Petr Zenin <mr@adminoid.com>
2022-02-06 21:19:17 +0300

    deposit USDT with BUSD tests successful

--- contracts/Matrix.sol
+++ contracts/Matrix.sol
@@ -7,22 +7,29 @@ import "hardhat/console.sol";
 
 contract Matrix is ERC20 {
     address USDTAddress;
+    address BUSDAddress;
     address BoxAddress;
-    address minter;
 
     IERC20 public USDTToken;
+    IERC20 public BUSDToken;
     IERC20 public MXToken;
 
-    constructor(address _USDTAddress, address _BoxAddress) ERC20("Matrix", "XUSD") {
-        USDTAddress = _USDTAddress;
+    constructor(address _BoxAddress, address _USDTAddress, address _BUSDAddress) ERC20("Matrix", "XUSD") {
         BoxAddress = _BoxAddress;
+        USDTAddress = _USDTAddress;
+        BUSDAddress = _BUSDAddress;
         MXToken = ERC20(address(this));
         USDTToken = ERC20(USDTAddress);
-        minter = msg.sender;
+        BUSDToken = ERC20(BUSDAddress);
     }
 
     function depositUSDT(uint _amount) payable public {
         USDTToken.transferFrom(msg.sender, BoxAddress, _amount);
+        _mint(msg.sender, _amount);
+    }
+
+    function depositBUSD(uint _amount) payable public {
+        BUSDToken.transferFrom(msg.sender, BoxAddress, _amount);
         _mint(msg.sender, _amount);
     }
 }
--- test/Matrix.js
+++ test/Matrix.js
@@ -5,17 +5,51 @@ const { deployContract } = waffle
 // contracts
 const Matrix = require('../artifacts/contracts/Matrix.sol/Matrix.json')
 const MockUSDT = require('../artifacts/contracts/mocks/mockUSDT.sol/MockUSDT.json')
-// const MockBUSD = require('../artifacts/contracts/mocks/mockBUSD.sol/MockBUSD.json')
+const MockBUSD = require('../artifacts/contracts/mocks/mockBUSD.sol/MockBUSD.json')
+
+// BUSD: https://testnet.bscscan.com/token/0xeD24FC36d5Ee211Ea25A80239Fb8C4Cfd80f12Ee
+// USDT: https://testnet.bscscan.com/token/0x337610d27c682E347C9cD60BD4b3b107C9d34dDd
+// BUSD and USDT all have 18 decimals
+
+const prepare = async () => {
+  const [
+    deployerUSDT,
+    deployerBUSD,
+    deployerMatrix,
+    commonWallet,
+    userWallet
+  ] = await ethers.getSigners()
+
+  const tokenUSDT = await deployContract(deployerUSDT, MockUSDT)
+  const tokenBUSD = await deployContract(deployerBUSD, MockBUSD)
+  const tokenMatrix = await deployContract(deployerMatrix, Matrix, [
+    commonWallet.address,
+    tokenUSDT.address,
+    tokenBUSD.address,
+  ])
+  return {
+    deployerUSDT,
+    deployerBUSD,
+    deployerMatrix,
+    commonWallet,
+    userWallet,
+    tokenUSDT,
+    tokenBUSD,
+    tokenMatrix,
+  }
+}
 
 describe('Deposit/withdraw BUSD and USDT with Matrix.sol', _ => {
-  it('Deposit USDT', async () => {
-    const [deployerUSDT, deployerMatrix, commonWallet, userWallet] = await ethers.getSigners()
 
-    const tokenUSDT = await deployContract(deployerUSDT, MockUSDT)
-    const tokenMatrix = await deployContract(deployerMatrix, Matrix, [
-      tokenUSDT.address,
-      commonWallet.address,
-    ])
+  it('Deposit and withdraw USDT', async () => {
+
+    const {
+      deployerUSDT,
+      commonWallet,
+      userWallet,
+      tokenUSDT,
+      tokenMatrix,
+    } = await prepare()
 
     await tokenUSDT.connect(deployerUSDT).transfer(userWallet.address, 11)
     let balance = await tokenUSDT.balanceOf(userWallet.address)
@@ -33,5 +67,31 @@ describe('Deposit/withdraw BUSD and USDT with Matrix.sol', _ => {
     let mxUserBalance = await tokenMatrix.balanceOf(userWallet.address)
     expect(mxUserBalance).to.equal(9)
 
+  }).timeout(20000)
+
+  it('Deposit and withdraw BUSD', async () => {
+    const {
+      deployerBUSD,
+      commonWallet,
+      userWallet,
+      tokenBUSD,
+      tokenMatrix,
+    } = await prepare()
+
+    await tokenBUSD.connect(deployerBUSD).transfer(userWallet.address, 11)
+    let balance = await tokenBUSD.balanceOf(userWallet.address)
+    expect(balance).to.equal(11) // ok
+
+    await tokenBUSD.connect(userWallet).approve(tokenMatrix.address, 9)
+    await tokenMatrix.connect(userWallet).depositBUSD(9)
+
+    let busdBalance = await tokenBUSD.balanceOf(userWallet.address)
+    expect(busdBalance).to.equal(2)
+
+    let mxTokenBalance = await tokenBUSD.balanceOf(commonWallet.address)
+    expect(mxTokenBalance).to.equal(9)
+
+    let mxUserBalance = await tokenMatrix.balanceOf(userWallet.address)
+    expect(mxUserBalance).to.equal(9)
   }).timeout(20000)
 })
