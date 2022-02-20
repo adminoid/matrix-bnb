54f52778d40ab4e75cf29c868e0f66904a8b3cd4
Petr Zenin <mr@adminoid.com>
2022-02-20 18:53:02 +0300

    script for parsing abi updated

--- scripts/get-abi.js
+++ scripts/get-abi.js
@@ -1,19 +1,28 @@
-const axios = require('axios');
+const axios = require('axios')
 const fs = require('fs')
 
 // https://api-testnet.bscscan.com/api?module=contract&action=getabi&address=
 // USDT 0x337610d27c682E347C9cD60BD4b3b107C9d34dDd
 // BUSD 0xeD24FC36d5Ee211Ea25A80239Fb8C4Cfd80f12Ee
 
-// const address = '0xeD24FC36d5Ee211Ea25A80239Fb8C4Cfd80f12Ee'
-const address = '0x337610d27c682E347C9cD60BD4b3b107C9d34dDd'
-// const fileName = 'scripts/output/busd.abi.json'
-const fileName = 'scripts/output/usdt.abi.json'
+// const currency = 'usdt' // `usdt` or `busd`
+const currency = 'busd' // `usdt` or `busd`
+let address, fileName
+
+if (currency === 'usdt') {
+  address = '0x337610d27c682E347C9cD60BD4b3b107C9d34dDd'
+  fileName = 'scripts/output/usdt.abi.json'
+} else if (currency === 'busd') {
+  address = '0xeD24FC36d5Ee211Ea25A80239Fb8C4Cfd80f12Ee'
+  fileName = 'scripts/output/busd.abi.json'
+} else {
+  throw new Error('invalid currency')
+}
 
 axios.get(`https://api-testnet.bscscan.com/api?module=contract&action=getabi&address=${address}`)
   .then(function (response) {
-    // console.log(JSON.stringify(response.data))
-    fs.writeFile(fileName, JSON.stringify(response.data), 'utf8', console.log);
+    // console.log(JSON.stringify(response.data.result))
+    fs.writeFile(fileName, JSON.stringify(response.data.result), 'utf8', console.log);
   })
   .catch(function (error) {
     console.log(error)
