3ad1cc3f42dfa9771dfbb81a314b53c3bb397257
Petr Zenin <mr@adminoid.com>
2024-11-20 17:40:35 +0300

    fixed "withdrawal (Referral)" block

--- test/Core.js
+++ test/Core.js
@@ -331,10 +331,10 @@ describe('practical testing interactions and that conclusions', async () => {
 
   it('check registration and resulting gifts and claims', async () => {
     // const users = await runRegistrations(44) // (19) regs -> 24 real
-    // const users = await runRegistrations(74) // (19) regs -> 24 real
-    const users = await runRegistrations(130) // (19) regs -> 24 real
-    // const users = await runRegistrations(150) // (19) regs -> 24 real
-    // const users = await runRegistrations(512) // (19) regs -> 24 real
+    // const users = await runRegistrations(74)
+    const users = await runRegistrations(130)
+    // const users = await runRegistrations(150)
+    // const users = await runRegistrations(512)
     // 63 real -5 = 58
     // 62
 
